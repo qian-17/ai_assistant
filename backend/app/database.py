c81d6776aeb6d03de85fc0944d3f4faebfd33860
qian-17 <519440749@qq.com>
2026-05-19 21:25:37 +0800

    fix mysql url

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,22 +6,12 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
-# 直接读取 Railway 提供的 DATABASE_URL
-DATABASE_URL = os.getenv("DATABASE_URL")
+# 直接读取 Railway MySQL_URL
+DATABASE_URL = os.getenv("MYSQL_URL")
 
 engine = create_engine(
     DATABASE_URL,
-
-    # 开发环境建议 True
-    # 部署生产环境建议 False
     echo=True,
-
-    # 连接池配置
-    pool_size=10,
-    max_overflow=20,
-    pool_recycle=3600,
-
-    # 自动检测失效连接（很重要）
     pool_pre_ping=True,
 )
 
@@ -29,8 +19,6 @@ SessionLocal = sessionmaker(
     autoflush=False,
     autocommit=False,
     bind=engine,
-
-    # 防止提交后对象失效
     expire_on_commit=False,
 )
 
